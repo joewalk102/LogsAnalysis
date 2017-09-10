b9d825cef7a2ddc7bb2cf9cf4200fc66791135b3
Joe Walker <joewalk102@gmail.com>
2017-09-09 21:04:19 -0700

    - Added print statement to methods
    - changed for loops for readability

--- main.py
+++ main.py
@@ -4,37 +4,37 @@ import query
 
 
 def find_top_articles():
+    print("Finding the top 3 most popular articles:\n")
     # getting the three most popular articles from the log
     popular_list = query.get_popular_articles()
     # cycle through all results
-    for article in popular_list:
+    for article, views in popular_list:
         # print the result
-        print(article[0] + " - " + str(article[1]) + " views")
+        print(str(article) + " - " + str(views) + " views")
 
 
 def find_top_authors():
+    print("\nFinding the total views, grouped by author:\n")
     # getting views by author, ordered by most viewed to least viewed
     results = query.get_all_article_count()
     # cycle through all results
-    for result in results:
+    for author, views in results:
         # print the result
-        print(str(result[1]) + " - " + str(result[0]) + " views")
+        print(str(author) + " - " + str(views) + " views")
 
 
 def find_error_percentage():
+    print("\nFinding the percent of errors, by day, where the error count is above 1%\n")
     # getting the error percent for any day where the error was above 1%
     results = query.get_error_requests()
     # cycle through all results
-    for result in results:
+    for date, percent in results:
         # print the result
-        print(str(result[0]) + " - " + str(result[1]) + "% errors")
+        print(str(date) + " - " + str(percent) + "% errors")
 
 
 if __name__ == '__main__':
     # Running the methods
-    print("Finding the top 3 most popular articles:\n")
     find_top_articles()
-    print("\nFinding the total views, grouped by author:\n")
     find_top_authors()
-    print("\nFinding the percent of errors, by day, where the error count is above 1%\n")
     find_error_percentage()
